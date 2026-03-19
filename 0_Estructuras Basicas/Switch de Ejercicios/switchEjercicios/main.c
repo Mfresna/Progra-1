d7244a96da0f4fb992de1a013d7f6fb474df7c26
Matias Fresnadillo <167906583+Mfresna@users.noreply.github.com>
2026-03-19 11:09:37 -0300

    TP1 - General
    TP1 - General

---
+++ 0_Estructuras Basicas/Switch de Ejercicios/switchEjercicios/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//PROTOTIPOS
+int menuOpciones();
+
+//MAIN
+int main()
+{
+    int opcion;
+
+    do {
+        opcion = menuOpciones();
+
+        switch(opcion) {
+            case 1:
+                //desarrollo del ejercicio 1
+                printf("Ejecucion Ej 1\n");
+                break;
+            case 2:
+                //desarrollo del ejercicio 2
+                printf("Ejecucion Ej 2\n");
+                break;
+            case 0:
+                printf("Usted ha salido del programa...\n");
+                break;
+            default:
+                printf("Opcion invalida\n");
+        }
+    }while(opcion != 0);
+
+    return 0;
+}
+
+//FUNCIONES
+int menuOpciones(){
+
+    int op;
+
+    printf("========== MENU ==========\n");
+    printf("1 - Ejercicio 1\n");
+    printf("2 - Ejercicio 2\n");
+    printf("3 - Ejercicio 3\n");
+
+    printf("\n0 - Salir\n");
+
+    printf("\nIngrese un numero: ");
+    scanf("%d", &op);
+
+    return op;
+
+}
---
+++ TP1-G-Punteros/Ejercicios_Punteros/main.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//PROTOTIPOS
+void menuOpciones(int *opcion);
+void cambiarSigno(int *num);
+void cargar(int *a, int *b);
+void validarEdad(int *edad);
+void dividir(int dividendo, int divisor, int *cociente, int *resto);
+void convertirTiempo(int segundos, int *minutos, int *horas);
+
+//MAIN
+int main()
+{
+    int opcion;
+
+    do {
+        menuOpciones(&opcion);
+
+        switch(opcion) {
+            case 1:
+                int numero;
+                printf("Ingrese un valor numerico:");
+                scanf("%d",&numero);
+
+                cambiarSigno(&numero);
+
+                printf("Ahora el numero es: %d", numero);
+
+                printf("\n\n");
+                break;
+            case 2:
+                int a;
+                int b;
+
+                cargar(&a,&b);
+
+                printf("El usuario cargo:");
+                printf("\n\t valor A: %d", a);
+                printf("\n\t valor B: %d", b);
+
+                printf("\n\n");
+                break;
+            case 3:
+                int edad;
+
+                validarEdad(&edad);
+
+                printf("\nLa edad final del usuario es: %d", edad);
+
+                printf("\n\n");
+                break;
+
+            case 4:
+                int dividendo;
+                int divisor;
+                int cociente;
+                int resto;
+
+                printf("\nDividir:");
+                scanf("%d",&dividendo);
+
+                printf("Entre:");
+                scanf("%d",&divisor);
+
+                dividir(dividendo, divisor, &cociente, &resto);
+
+                printf("\nEl resultado es: %d y su resto es: %d",cociente, resto);
+
+                printf("\n\n");
+                break;
+            case 5:
+                int segundos = 0;
+                int minutos;
+                int horas;
+
+                printf("\nSegundos a convertir:");
+                scanf("%d",&segundos);
+
+                convertirTiempo(segundos, &minutos ,&horas);
+
+                printf("\nSon %d horas o %d minutos",horas, minutos);
+
+                printf("\n\n");
+                break;
+            case 0:
+                printf("Usted ha salido del programa...\n");
+                break;
+            default:
+                printf("Opcion invalida\n");
+        }
+    }while(opcion != 0);
+
+    return 0;
+}
+
+//FUNCIONES
+void menuOpciones(int *opcion){
+
+    printf("========== MENU ==========\n");
+    printf("1 - Cambiar signo usando referencia\n");
+    printf("2 - Cargar dos números por referencia\n");
+    printf("3 - Validar edad con puntero\n");
+    printf("4 - Dividir con resultado y resto (dos return)\n");
+    printf("5 - Segundos a Horas y Minutos\n");
+
+    printf("\n0 - Salir\n");
+
+    printf("\nIngrese un numero: ");
+    scanf("%d", opcion);
+}
+
+    //Ejercicio 1
+void cambiarSigno(int *num) {
+    *num = -(*num);
+}
+
+    //Ejercicio 2
+void cargar(int *a, int *b) {
+    printf("Ingrese el primer numero: ");
+    scanf("%d", a);
+
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", b);
+}
+
+    //Ejercicio 3
+void validarEdad(int *edad) {
+    do {
+        printf("Ingrese edad (1 a 18): ");
+        scanf("%d", edad);
+
+        if (*edad <= 0 || *edad > 18) {
+            printf("Edad incorrecta. Debe ser mayor a 0 y menor o igual a 18.\n");
+        }
+
+    } while (*edad <= 0 || *edad > 18);
+}
+
+    //Ejercicio 4
+void dividir(int dividendo, int divisor, int *cociente, int *resto) {
+    *cociente = dividendo / divisor;
+    *resto = dividendo % divisor;
+}
+
+    //Ejercicio 5
+void convertirTiempo(int segundos, int *minutos, int *horas) {
+    *minutos = segundos / 60;
+    *horas = segundos / 3600;
+}
+
